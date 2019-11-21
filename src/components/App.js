14b633893050b26ccb8d131b16cb5206af57c110
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-21 14:47:32 -0500

    Post mutation added, list not updating

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import LinkList from "./LinkList";
+import CreateLink from "./CreateLink";
 
-const App = () => <LinkList />;
+const App = () => {
+  return (
+    <>
+      <LinkList />
+      <CreateLink />
+    </>
+  );
+};
 
 export default App;
