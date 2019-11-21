49501ce762393469874ac0fca28c389b01229883
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-21 13:40:04 -0500

    Set up Urql and exchanges

--- src/index.js
+++ src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider, Client, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
+
+const cache = cacheExchange({});
+
+const client = new Client({
+  url: "http://localhost:4000",
+  exchanges: [dedupExchange, cache, fetchExchange]
+});
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
