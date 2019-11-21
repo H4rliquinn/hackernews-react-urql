f8ea303bfc6779f0255dd3331c283f1d53a71f53
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-21 15:15:31 -0500

    User create/login added, need to add token to server

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import { Provider, Client, dedupExchange, fetchExchange } from "urql";
 import { cacheExchange } from "@urql/exchange-graphcache";
+import { getToken } from "./token";
 
 const cache = cacheExchange({});
 
 const client = new Client({
   url: "http://localhost:4000",
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : "" }
+    };
+  },
   exchanges: [dedupExchange, cache, fetchExchange]
 });
 
