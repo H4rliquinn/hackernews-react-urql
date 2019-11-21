91dbc6d732e3cb849fb888bdeb051537fbb1935d
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-21 17:18:59 -0500

    Added Search

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,17 @@ import Header from "./Header";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
 import Login from "./Login";
+import Search from "./Search";
 
 const App = () => (
   <div className="center w85">
     <Header />
     <div className="ph3 pv1 background-gray">
       <Switch>
-        <Route exact path="/" component={LinkList} />
         <Route exact path="/create" component={CreateLink} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/search" component={Search} />
+        <Route path="/" component={LinkList} />
       </Switch>
     </div>
   </div>
