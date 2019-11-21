e91535909183aa28cbfc46ca1b511cbf18eff7e4
Jason Sonnichsen <Harliquinn@gmail.com>
2019-11-21 14:20:31 -0500

    Useing Urql to fill Links List

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,25 +1,51 @@
 import React from "react";
 import Link from "./Link";
+import { useQuery } from "urql";
+import gql from "graphql-tag";
 
-const linksToRender = [
+// const linksToRender = [
+//   {
+//     id: "1",
+//     description: "Prisma turns your database into a GraphQL API 😎",
+//     url: "https://www.prismagraphql.com"
+//   },
+//   {
+//     id: "2",
+//     description: "The best GraphQL client",
+//     url: "https://formidable.com/open-source/urql/"
+//   }
+// ];
+
+const FEED_QUERY = gql`
   {
-    id: "1",
-    description: "Prisma turns your database into a GraphQL API 😎",
-    url: "https://www.prismagraphql.com"
-  },
-  {
-    id: "2",
-    description: "The best GraphQL client",
-    url: "https://formidable.com/open-source/urql/"
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
   }
-];
+`;
 
-const LinkList = () => (
-  <div>
-    {linksToRender.map(link => (
-      <Link key={link.id} link={link} />
-    ))}
-  </div>
-);
+const LinkList = () => {
+  const [result] = useQuery({ query: FEED_QUERY });
+  console.log("RES", result);
+  const { data, fetching, error } = result;
+  console.log("DATA", data);
+  if (fetching) return <div>Fetching</div>;
+  if (error) return <div>Error</div>;
+
+  const linksToRender = data.feed.links;
+
+  return (
+    <div>
+      {linksToRender.map(link => (
+        <Link key={link.id} link={link} />
+      ))}
+    </div>
+  );
+};
 
 export default LinkList;
